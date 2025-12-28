4ff45c8842bf9580598c5deff651d3cb88d89c5a
李振宇 <407852978@qq.com>
2025-12-28 22:47:03 +0800

    update init

--- R68S/init.sh
+++ R68S/init.sh
@@ -1,3 +1,3 @@
-wget https://raw.githubusercontent.com/happyplum/action-openwrt/refs/heads/main/R68S/feeds.conf.default -O feeds.conf.default
+wget https://raw.githubusercontent.com/happyplum/action-openwrt/refs/heads/main/diy-part1.sh -O diy-part1.sh
 wget https://raw.githubusercontent.com/happyplum/action-openwrt/refs/heads/main/R68S/diy-part2.sh -O diy-part2.sh
 wget https://raw.githubusercontent.com/happyplum/action-openwrt/refs/heads/main/R68S/.config -O .config
