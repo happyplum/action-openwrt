8abf6aef0b534104c5a36ca4bcd51a7e457a399d
李振宇 <407852978@qq.com>
2026-01-28 00:33:57 +0800

    update

--- R68S/diy-part2.sh
+++ R68S/diy-part2.sh
@@ -8,19 +8,6 @@
 
 echo "开始 DIY2 配置……"
 echo "========================="
-
-function merge_package(){
-    repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
-    pkg=`echo $2 | rev | cut -d'/' -f 1 | rev`
-    # find package/ -follow -name $pkg -not -path "package/custom/*" | xargs -rt rm -rf
-    git clone --depth=1 --single-branch $1
-    mv $2 package/custom/
-    rm -rf $repo
-}
-function drop_package(){
-    find package/ -follow -name $1 -not -path "package/custom/*" | xargs -rt rm -rf
-}
-
 # ------------------------------- Custom Package -------------------------------
 ./scripts/feeds update -a
 # ------------------------------- Custom Package -------------------------------
