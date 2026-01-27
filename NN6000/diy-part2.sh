41807ba81d789f4a06e354b60555cce084afbf2f
李振宇 <407852978@qq.com>
2026-01-28 00:35:36 +0800

    update

--- NN6000/diy-part2.sh
+++ NN6000/diy-part2.sh
@@ -8,36 +8,7 @@
 
 echo "开始 DIY2 配置……"
 echo "========================="
-
-function merge_package(){
-    repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
-    pkg=`echo $2 | rev | cut -d'/' -f 1 | rev`
-    # find package/ -follow -name $pkg -not -path "package/custom/*" | xargs -rt rm -rf
-    git clone --depth=1 --single-branch $1
-    mv $2 package/custom/
-    rm -rf $repo
-}
-function drop_package(){
-    find package/ -follow -name $1 -not -path "package/custom/*" | xargs -rt rm -rf
-}
-function merge_feed(){
-    if [ ! -d "feed/$1" ]; then
-        echo >> feeds.conf.default
-        echo "src-git $1 $2" >> feeds.conf.default
-    fi
-    ./scripts/feeds update $1
-    ./scripts/feeds install -a -p $1
-}
-rm -rf package/custom; mkdir package/custom
-
-
 # ------------------------------- Custom Package -------------------------------
-# passwall
-merge_package https://github.com/xiaorouji/openwrt-passwall openwrt-passwall/luci-app-passwall
-
-# 2025年12月17日 尝试使用src-link来使用merge-package/custom下载的smartdns
-merge_package https://github.com/pymumu/luci-app-smartdns luci-app-smartdns
-
 ./scripts/feeds update -a
 # ------------------------------- Custom Package -------------------------------
 
@@ -50,10 +21,42 @@ ip_regex="^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01
     echo "Modify default IP address to: ${1}"
     sed -i "/lan) ipad=\${ipaddr:-/s/\${ipaddr:-\"[^\"]*\"}/\${ipaddr:-\"${1}\"}/" package/base-files/*/bin/config_generate
 }
+
+# 优化
+rm -rf package/base-files/files/etc/sysctl.d/0-base.conf
+wget -P package/base-files/files/etc/sysctl.d https://raw.githubusercontent.com/happyplum/action-openwrt/refs/heads/main/config/0-base.conf
+rm -rf package/base-files/files/etc/sysctl.d/0-v4.conf
+wget -P package/base-files/files/etc/sysctl.d https://raw.githubusercontent.com/happyplum/action-openwrt/refs/heads/main/config/0-v4.conf
+rm -rf package/base-files/files/etc/sysctl.d/0-v6.conf
+wget -P package/base-files/files/etc/sysctl.d https://raw.githubusercontent.com/happyplum/action-openwrt/refs/heads/main/config/0-v6.conf
+rm -rf package/base-files/files/etc/sysctl.d/1-netfilter.conf
+wget -P package/base-files/files/etc/sysctl.d https://raw.githubusercontent.com/happyplum/action-openwrt/refs/heads/main/config/1-netfilter.conf
+
+# 下载v2ray的dat数据
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf package/base-files/files/usr/share/v2ray/geoip.dat
+wget -P package/base-files/files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
+rm -rf package/base-files/files/usr/share/v2ray/geosite.dat
+wget -P package/base-files/files/usr/share/v2ray https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+rm -rf package/base-files/files/usr/share/xray
+mkdir package/base-files/files/usr/share/xray
+ln -s package/base-files/files/usr/share/v2ray/geoip.dat package/base-files/files/usr/share/xray/geoip.dat
+ln -s package/base-files/files/usr/share/v2ray/geosite.dat package/base-files/files/usr/share/xray/geosite.dat
 # ------------------------------- Main source ends -------------------------------
 
 # ------------------------------- Other started -------------------------------
 #
+# 1.设置OpenWrt 文件的下载仓库
+# mosdns
+rm -rf package/mosdns
+git clone --depth=1 -b v5 --single-branch https://github.com/sbwml/luci-app-mosdns package/mosdns
+
+#smartdns
+rm -rf package/luci-app-smartdns
+rm -rf feeds/packages/net/smartdns
+git clone --depth=1 --single-branch https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+git clone --depth=1 --single-branch https://github.com/pymumu/openwrt-smartdns feeds/packages/net/smartdns
+
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
